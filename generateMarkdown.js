028bdf5139b0d26376563b869fae25d3a54926a9
CatSink <raindropwebweaver@live.com>
2023-03-28 22:29:47 -0400

    added if statements for license and badges to gen file, added questions to index.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === 'MIT')
+    return 'https://img.shields.io/badge/badge-MIT-blue'
+    if (license === 'Apahe')
+    return 'https://img.shields.io/badge/badge-APACHE-orange'
+    if (license === 'BSD')
+    return 'https://img.shields.io/badge/badge-BSD-red'
+    else if (license === 'Unlicensed')
+    return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === 'MIT')
+    return 'https://opensource.org/license/mit/'
+    if (license === 'Apahe')
+    return 'https://opensource.org/license/apache-2-0/'
+    if (license === 'BSD')
+    return 'https://opensource.org/license/bsd-3-clause/'
+    else if (license === 'Unlicensed')
+    return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type:'input',
+      prompt: 'what is your project title?',
+      name: 'title'
+    },
+    {
+      type: 'input',
+      prompt: 'describe your project',
+      name: 'description'   
+    },
+    {
+      type: 'input',
+      prompt: 'what are the steps for installing your project?',
+      name: 'installation'
+    },
+    {
+      type: 'input',
+      prompt: 'please provide instructions and visual explamples for utilizing your project',
+      name: 'Usage'
+    },
+    {
+      type: 'input',
+      prompt: 'please list any collaboartors and provide links to third-party resources',
+      name: 'credits'
+    },    
+    {
+     type: 'input',
+     prompt: 'what type of license does your project have?',
+     name: 'license'  
+    },
+    {
+      type: 'input',
+      prompt: 'list your projects features',
+      name: 'features'
+    },
+    
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
